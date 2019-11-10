7d04254dbe047f6d48af581bfefbced7ccd52caa
egeerdogan <ege@erdogan.dev>
2019-11-10 10:51:34 +0300

    physics engine signatures;

--- src/brickingbad/domain/physics/PhysicsEngine.java
+++ src/brickingbad/domain/physics/PhysicsEngine.java
@@ -1,4 +1,25 @@
 package brickingbad.domain.physics;
 
 public class PhysicsEngine {
+
+  public static void handleCollisions() {
+
+  }
+
+  public static void updatePositions() {
+
+  }
+
+  private static boolean areColliding(GameObject o1, GameObject o2) {
+
+  }
+
+  private static List<List<GameObject>> checkCollisions(List<GameObject> objects) {
+
+  }
+
+  private static void performCollisions(List<List<GameObject>> collidingPairs) {
+
+  }
+
 }
