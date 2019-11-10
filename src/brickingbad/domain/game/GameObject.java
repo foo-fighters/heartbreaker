39adf9bd259221b2216b996683429bd68fab0612
egeerdogan <ege@erdogan.dev>
2019-11-10 10:36:48 +0300

    paddle class moved to game domain

--- src/brickingbad/domain/game/GameObject.java
+++ src/brickingbad/domain/game/GameObject.java
@@ -1,4 +1,11 @@
 package brickingbad.domain.game;
 
 public abstract  class GameObject {
+
+  public void updatePosition() { }
+
+  public void reflect(GameObject object) { }
+
+  public void destroy() { }
+
 }
--- src/brickingbad/domain/game/Paddle.java
+++ src/brickingbad/domain/game/Paddle.java
@@ -1,4 +1,41 @@
 package brickingbad.domain.game;
 
+import brickingbad.domain.physics.paddle.Direction;
+import brickingbad.domain.physics.paddle.PaddleMoveState;
+
 public class Paddle extends GameObject {
+
+  private Ball[] currentBalls;
+  private PaddleMoveState moveState;
+  private PaddleMoveState rotateState;
+  private double angle;
+  private int angularVelocity;
+  private boolean isMagnetized;
+
+  public void launchBalls() { }
+
+
+  public void updatePosition() { }
+
+  public void reflect(GameObject object) { }
+
+  public void destroy() { }
+
+
+  public void startMove(Direction direction) { }
+
+  public void endMove(Direction direction){ }
+
+  public void setIdleMove(){ }
+
+
+  public void startRotate(Direction direction){ }
+
+  public void endRotate(Direction direction) { }
+
+  public void setIdleRotate(){ }
+
+
+  private void clearBalls() { }
+
 }
