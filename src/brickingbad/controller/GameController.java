c4aa7251c3a0aa9077501f94f515b10087c81348
egeerdogan <ege@erdogan.dev>
2019-11-10 15:54:53 +0300

    game controller method stubs + singleton implementation

--- src/brickingbad/controller/GameController.java
+++ src/brickingbad/controller/GameController.java
@@ -1,4 +1,30 @@
 package brickingbad.controller;
 
+import brickingbad.domain.physics.paddle.Direction;
+
 public class GameController {
+
+  private GameController instance;
+
+  public GameController getInstance() {
+    if (instance == null) {
+      instance = new GameController();
+    }
+    return instance;
+  }
+
+  public void startGame() { }
+
+
+  public void launchBalls() { }
+
+
+  public void startPaddleMove(Direction direction) { }
+
+  public void endPaddleMove(Direction direction) { }
+
+  public void startPaddleRotate(Direction direction) { }
+
+  public void endPaddleRotate(Direction direction) { }
+
 }
