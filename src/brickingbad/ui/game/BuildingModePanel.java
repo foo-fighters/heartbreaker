81ca55c1ec97de1171df563d40c8501905033f0e
egeerdogan <ege@erdogan.dev>
2019-11-14 22:04:59 +0300

    building mode panel remove singleton

--- src/brickingbad/ui/game/BuildingModePanel.java
+++ src/brickingbad/ui/game/BuildingModePanel.java
@@ -4,21 +4,9 @@ import javax.swing.*;
 
 public class BuildingModePanel extends JPanel {
 
-  private static BuildingModePanel instance;
-
-  private BuildingModePanel() { };
-
-  public static BuildingModePanel getInstance() {
-    if (instance == null) {
-      instance = new BuildingModePanel();
-    }
-    initialize();
-    return instance;
-  }
-
-  private static void initialize() {
+  public BuildingModePanel() {
     JLabel tempLabel = new JLabel("THIS IS THE BUILDING MODE!");
-    instance.add(tempLabel);
+    add(tempLabel);
   }
 
 }
