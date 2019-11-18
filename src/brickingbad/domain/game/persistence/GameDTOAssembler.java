32fca1b54882c38f80935fc8b203537189250e1b
egeerdogan <ege@erdogan.dev>
2019-11-18 22:47:47 +0300

    imports

--- src/brickingbad/domain/game/persistence/GameDTOAssembler.java
+++ src/brickingbad/domain/game/persistence/GameDTOAssembler.java
@@ -1,13 +1,12 @@
 package brickingbad.domain.game.persistence;
 
-import brickingbad.domain.game.Ball;
 import brickingbad.domain.game.Game;
 
-import java.util.ArrayList;
 
 public class GameDTOAssembler {
 
   public static GameDTO assemble(Game game) {
+    // TODO
     return null;
   }
 
