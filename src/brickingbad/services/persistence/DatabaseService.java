2e830aad4c2e51079a58f296c04bd0a89b6a218a
egeerdogan <ege@erdogan.dev>
2019-11-18 20:30:26 +0300

    setup database service

--- src/brickingbad/services/persistence/DatabaseService.java
+++ src/brickingbad/services/persistence/DatabaseService.java
@@ -2,15 +2,21 @@ package brickingbad.services.persistence;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 public class DatabaseService {
 
-  MongoClientURI uri = new MongoClientURI(
+  private static MongoClientURI uri = new MongoClientURI(
           "mongodb+srv://admin:comp302@bricking-bad-lw1qn.mongodb.net/test?retryWrites=true&w=majority");
 
-  MongoClient mongoClient = new MongoClient(uri);
-  MongoDatabase database = mongoClient.getDatabase("test");
+  private static MongoClient mongoClient = new MongoClient(uri);
+  private static MongoDatabase database = mongoClient.getDatabase("brickingBad");
+
+  private static MongoCollection<Document> gamesCollection = database.getCollection("games");
+  private static MongoCollection<Document> usersCollection = database.getCollection("users");
+
 
 
 }
