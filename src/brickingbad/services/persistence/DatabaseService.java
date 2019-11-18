b4e8b0b9e8cad71eb03feeece18ca73c369f8226
egeerdogan <ege@erdogan.dev>
2019-11-18 20:40:27 +0300

    add codec registry

--- src/brickingbad/services/persistence/DatabaseService.java
+++ src/brickingbad/services/persistence/DatabaseService.java
@@ -5,18 +5,22 @@ import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class DatabaseService {
 
   private static MongoClientURI uri = new MongoClientURI(
           "mongodb+srv://admin:comp302@bricking-bad-lw1qn.mongodb.net/test?retryWrites=true&w=majority");
 
+  private static CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
+                                                   fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+
   private static MongoClient mongoClient = new MongoClient(uri);
-  private static MongoDatabase database = mongoClient.getDatabase("brickingBad");
-
-  private static MongoCollection<Document> gamesCollection = database.getCollection("games");
-  private static MongoCollection<Document> usersCollection = database.getCollection("users");
-
-
+  private static MongoDatabase database = mongoClient.getDatabase("brickingBad")
+                                                     .withCodecRegistry(pojoCodecRegistry);
 
 }
