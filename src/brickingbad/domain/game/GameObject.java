71233ab0f28b705f641fc56f56c581daf009940b
egeerdogan <ege@erdogan.dev>
2019-11-18 22:59:04 +0300

    gameobject getters/setters

--- src/brickingbad/domain/game/GameObject.java
+++ src/brickingbad/domain/game/GameObject.java
@@ -2,7 +2,7 @@ package brickingbad.domain.game;
 
 import brickingbad.domain.physics.Vector;
 
-public abstract  class GameObject {
+public abstract class GameObject {
 
   protected Shape shape;
   protected Vector size;
@@ -16,5 +16,37 @@ public abstract  class GameObject {
   public void reflect(GameObject object) { }
 
   public void destroy() { }
+
+  public Shape getShape() {
+    return shape;
+  }
+
+  public void setShape(Shape shape) {
+    this.shape = shape;
+  }
+
+  public Vector getSize() {
+    return size;
+  }
+
+  public void setSize(Vector size) {
+    this.size = size;
+  }
+
+  public Vector getPosition() {
+    return position;
+  }
+
+  public void setPosition(Vector position) {
+    this.position = position;
+  }
+
+  public Vector getVelocity() {
+    return velocity;
+  }
+
+  public void setVelocity(Vector velocity) {
+    this.velocity = velocity;
+  }
 
 }
--- src/brickingbad/domain/game/persistence/GameDTOAssembler.java
+++ src/brickingbad/domain/game/persistence/GameDTOAssembler.java
@@ -1,13 +1,34 @@
 package brickingbad.domain.game.persistence;
 
+import brickingbad.domain.game.Ball;
 import brickingbad.domain.game.Game;
+import brickingbad.domain.game.brick.Brick;
+
+import java.util.ArrayList;
 
 
 public class GameDTOAssembler {
 
   public static GameDTO assemble(Game game) {
-    // TODO
-    return null;
+    int score = game.getScore();
+    int lives = game.getLives();
+    int paddleX = game.getPaddle().getPosition().getX();
+    int paddleY = game.getPaddle().getPosition().getY();
+
+    ArrayList<Ball> balls = game.getBalls();
+    int ballCount = balls.size();
+    int[] ballX = new int[ballCount];
+    int[] ballY = new int[ballCount];
+    for (int i = 0; i < ballCount; i++) {
+      ballX[i] = balls.get(i)
+    }
+
+
+    ArrayList<Brick> bricks = game.getBricks();
+    int brickCount = bricks.size();
+    int[] brickX = new int[brickCount];
+    int[] brickY = new int[brickCount];
+
   }
 
 }
