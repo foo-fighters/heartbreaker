183fcb19cc0ddc8774856bd036cd68dd79d42a30
egeerdogan <ege@erdogan.dev>
2019-11-18 22:52:19 +0300

    fix ballstate reflect method visibility

--- src/brickingbad/domain/physics/ball/BallState.java
+++ src/brickingbad/domain/physics/ball/BallState.java
@@ -1,10 +1,9 @@
 package brickingbad.domain.physics.ball;
 
-import brickingbad.domain.game.Ball;
 import brickingbad.domain.game.GameObject;
 
 public abstract class BallState {
 
-    abstract void reflect(GameObject object);
+    public abstract void reflect(GameObject object);
 
 }
