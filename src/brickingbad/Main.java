c4474e8708ffb65979e9ce5914e389c8b605e9f6
egeerdogan <ege@erdogan.dev>
2019-11-21 14:03:18 +0300

    mac ui fix

--- src/brickingbad/Main.java
+++ src/brickingbad/Main.java
@@ -9,7 +9,13 @@ public class Main {
   public static void main(String[] args) {
     // Runs the code on the AWT thread. Which lets you modify the GUI from other threads.
     SwingUtilities.invokeLater(() ->
-            BrickingBadFrame.getInstance());
+      BrickingBadFrame.getInstance());
+    try {
+      UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
   }
 
 }
