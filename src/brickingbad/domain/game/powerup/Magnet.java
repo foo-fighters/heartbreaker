d929c57e8f939d94e3e12e499cc1a5ca4c58f1ac
BATUHAN YUCER <byucer15@ku.edu.tr>
2019-11-26 21:17:26 +0300

    add powerup constructors

--- src/brickingbad/domain/game/powerup/Magnet.java
+++ src/brickingbad/domain/game/powerup/Magnet.java
@@ -1,9 +1,17 @@
 package brickingbad.domain.game.powerup;
 
+import brickingbad.domain.game.GameConstants;
+import brickingbad.domain.game.Shape;
 import brickingbad.domain.physics.Vector;
 
 public class Magnet extends PowerUp {
+
     public Magnet(Vector revealPosition) {
+        this.position = new Vector(revealPosition.getX(), revealPosition.getY());
+        this.shape = Shape.RECTANGLE;
+        this.size = new Vector(GameConstants.powerupSize, GameConstants.powerupSize);
+        this.velocity = new Vector(0, GameConstants.powerupFallSpeed);
+        this.angle = 0.0;
     }
 
     @Override
