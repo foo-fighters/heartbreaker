15146c9eecfa06bd3a87b4181e20012b97c4fe87
egeerdogan <ege@erdogan.dev>
2019-12-04 13:41:59 +0300

    readw write from local files

--- src/brickingbad/services/persistence/LocalPersistenceAdapter.java
+++ src/brickingbad/services/persistence/LocalPersistenceAdapter.java
@@ -1,18 +1,31 @@
 package brickingbad.services.persistence;
 
 import brickingbad.domain.game.persistence.Save;
+import brickingbad.domain.game.persistence.SaveAssembler;
 import com.google.gson.Gson;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LocalPersistenceAdapter implements IPersistenceAdapter {
 
+  private final String savePath = "storage/saves/";
+
   @Override
   public Save getSaveByName(String name) {
-
+    Save save = null;
+    try {
+      String json = Files.readString(Paths.get(savePath + name + ".json"), StandardCharsets.US_ASCII);
+      Gson gson = new Gson();
+      save = gson.fromJson(json, Save.class);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return save;
   }
 
   @Override
@@ -20,7 +33,7 @@ public class LocalPersistenceAdapter implements IPersistenceAdapter {
     Gson gson = new Gson();
     String json = gson.toJson(save);
     try {
-      BufferedWriter writer = new BufferedWriter(new FileWriter("storage/saves/" + save.name + ".json"));
+      BufferedWriter writer = new BufferedWriter(new FileWriter(savePath + save.name + ".json"));
       writer.write(json);
       writer.close();
     } catch (IOException e) {
@@ -30,7 +43,15 @@ public class LocalPersistenceAdapter implements IPersistenceAdapter {
 
   @Override
   public List<String> getSaveNames() {
-
+    ArrayList<String> names = new ArrayList<>();
+    File folder = new File(savePath);
+    File[] listOfFiles = folder.listFiles();
+    for (File file : listOfFiles) {
+      String fullName = file.getName();
+      String[] tokens = fullName.split("[.]");
+      names.add(tokens[0]);
+    }
+    return names;
   }
 
 }
