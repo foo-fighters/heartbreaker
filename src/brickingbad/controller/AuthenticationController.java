9e566d1746c5d70e970c52a7b265a11e2d48bb08
egeerdogan <ege@erdogan.dev>
2019-12-05 00:01:39 +0300

    implement auth controller

--- src/brickingbad/controller/AuthenticationController.java
+++ src/brickingbad/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package brickingbad.controller;
 
 import brickingbad.domain.game.authentication.User;
+import brickingbad.services.Adapter;
 import brickingbad.services.authentication.UserRepository;
 
 public class AuthenticationController {
@@ -20,14 +21,27 @@ public class AuthenticationController {
     return instance;
   }
 
-  public boolean authenticate() {
-    return true;
+  public boolean authenticate(String name, String password) {
+    try {
+      User user = userRepository.findUserByName(name);
+      return user.password.equals(password);
+    } catch (IllegalArgumentException e) {
+      // TODO: handle (show UI message - failed login)
+      return false;
+    }
   }
 
   public void addUser(User user) {
-    userRepository.addUser(user);
+    try {
+      userRepository.addUser(user);
+    } catch (IllegalArgumentException e) {
+      // TODO: handle (show UI message)
+    }
   }
 
-
+  public AuthenticationController adapt(Adapter adapter) {
+    userRepository.adapt(adapter);
+    return this;
+  }
 
 }
