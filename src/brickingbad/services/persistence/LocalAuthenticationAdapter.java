eb1ff1bc422a8ba969ed8a480671e35237b21f0a
egeerdogan <ege@erdogan.dev>
2019-12-04 23:42:57 +0300

    implement local auth adapter

--- src/brickingbad/services/persistence/LocalAuthenticationAdapter.java
+++ src/brickingbad/services/persistence/LocalAuthenticationAdapter.java
@@ -1,4 +1,74 @@
 package brickingbad.services.persistence;
 
+import brickingbad.domain.game.authentication.User;
+import brickingbad.services.encryption.Decoder;
+import brickingbad.services.encryption.Encoder;
+import com.google.gson.Gson;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LocalAuthenticationAdapter implements IAuthenticationAdapter {
+
+  private final String usersPath = "storage/users/";
+
+  @Override
+  public void addUser(User user) throws IllegalArgumentException {
+    ArrayList<String> names = getNames();
+    if (names.contains(user.name)) {
+      throw new IllegalArgumentException("A user with that username already exists.");
+    } else {
+      String encodedPassword = Encoder.encodeString(user.password);
+      String encodedName = Encoder.encodeString(user.name);
+      try {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(usersPath + encodedName + ".txt"));
+        writer.write(encodedPassword);
+        writer.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  @Override
+  public User findUserByName(String name) throws IllegalArgumentException {
+    List<String> names = getNames()
+                              .stream().map((userName) -> Decoder.decodeString(userName))
+                              .collect(Collectors.toList());
+    if (!names.contains(name)) {
+      throw new IllegalArgumentException("A user with that name doesn't exist.");
+    } else {
+      try {
+        String encodedPassword = Files.readString(Paths.get(usersPath + Encoder.encodeString(name) + ".txt"), StandardCharsets.US_ASCII);
+        String decodedPassword = Decoder.decodeString(encodedPassword);
+        User user = new User(name, decodedPassword);
+        return user;
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public ArrayList<String> getNames() {
+    ArrayList<String> names = new ArrayList<>();
+    File folder = new File(usersPath);
+    File[] listOfFiles = folder.listFiles();
+    for (File file : listOfFiles) {
+      String fullName = file.getName();
+      String[] tokens = fullName.split("[.]");
+      names.add(tokens[0]);
+    }
+    return names;
+  }
+
 }
