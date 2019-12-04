2cad0521f1bb38e112a4ecf6345eb3e31fc10754
egeerdogan <ege@erdogan.dev>
2019-12-05 01:09:49 +0300

    user authentication complete

--- src/brickingbad/domain/game/authentication/User.java
+++ src/brickingbad/domain/game/authentication/User.java
@@ -5,6 +5,10 @@ public class User {
   public String name;
   public String password;
 
+  public User() {
+
+  }
+
   public User(String name, String password) {
     this.name = name;
     this.password = password;
--- src/brickingbad/ui/menu/LandingPanel.java
+++ src/brickingbad/ui/menu/LandingPanel.java
@@ -1,9 +1,14 @@
 package brickingbad.ui.menu;
 
+import brickingbad.controller.AuthenticationController;
+import brickingbad.domain.game.GameConstants;
+import brickingbad.domain.game.authentication.User;
+import brickingbad.services.Adapter;
 import brickingbad.ui.BrickingBadFrame;
 import brickingbad.ui.components.BBMenuButton;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,13 +18,37 @@ public class LandingPanel extends JPanel implements ActionListener {
 
   private final BrickingBadFrame bbFrame = BrickingBadFrame.getInstance();
 
+  private static JLabel usernameLabel;
+  private static JLabel passwordLabel;
+  private static JTextField usernameField;
+  private static JTextField passwordField;
+
+  private static JComboBox adapterSelectionBox;
+
   private static BBMenuButton registerButton;
   private static BBMenuButton loginButton;
 
   private LandingPanel() {
+//    setLayout(new GridLayout(3, 5));
+    setSize(GameConstants.screenWidth / 2, GameConstants.screenHeight / 2);
+
+    usernameLabel = new JLabel("USERNAME");
+    passwordLabel = new JLabel("PASSWORD");
+
+    usernameField = new JTextField("", 20);
+    passwordField = new JTextField("", 20);
+
+    String[] selections = {Adapter.LOCAL.name(), Adapter.MONGODB.name()};
+    adapterSelectionBox = new JComboBox(selections);
+
     registerButton = new BBMenuButton("CREATE AN ACCOUNT", this);
     loginButton = new BBMenuButton("LOG IN", this);
 
+    add(usernameLabel);
+    add(usernameField);
+    add(passwordLabel);
+    add(passwordField);
+    add(adapterSelectionBox);
     add(registerButton);
     add(loginButton);
   }
@@ -33,10 +62,15 @@ public class LandingPanel extends JPanel implements ActionListener {
 
   @Override
   public void actionPerformed(ActionEvent e) {
+    String name = usernameField.getText();
+    String password = passwordField.getText();
+    String adapter = (String) adapterSelectionBox.getSelectedItem();
     if (e.getSource().equals(registerButton)) {
-      bbFrame.showRegisterDialog();
+      User user = new User(name, password);
+      AuthenticationController.getInstance().adapt(Adapter.valueOf(adapter)).addUser(user);
     } else if (e.getSource().equals(loginButton)) {
-      bbFrame.showLoginDialog();
+      boolean auth = AuthenticationController.getInstance().adapt(Adapter.valueOf(adapter)).authenticate(name, password);
+      System.out.println(auth);
     }
   }
 
