f04e598aed45388a91ab8443e02c1b3b82de0d24
egeerdogan <ege@erdogan.dev>
2019-12-06 13:08:56 +0300

    backgrounds

--- src/brickingbad/ui/game/RunningModePanel.java
+++ src/brickingbad/ui/game/RunningModePanel.java
@@ -96,7 +96,7 @@ public class RunningModePanel extends JPanel implements GameObjectListener {
   protected void paintComponent(Graphics g) {
 
     super.paintComponent(g);
-    g.drawImage(background, 0, 0, null);
+    g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
     for (UIGameObject object : uiObjects) {
       object.paintComponent(g);
     }
