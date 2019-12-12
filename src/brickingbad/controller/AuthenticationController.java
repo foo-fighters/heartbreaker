767688dbc7e7ac03c8f3e328ddcc4bc5da2fe981
egeerdogan <ege@erdogan.dev>
2019-12-12 17:01:18 +0300

    authentication controller documentation

--- src/brickingbad/controller/AuthenticationController.java
+++ src/brickingbad/controller/AuthenticationController.java
@@ -4,18 +4,35 @@ import brickingbad.domain.game.authentication.User;
 import brickingbad.services.Adapter;
 import brickingbad.services.authentication.UserRepository;
 
+/**
+ * AuthenticationController is responsible of handling requests related to authentication.
+ *
+ */
 public class AuthenticationController {
 
+  /**
+   * Maintains the single instance of the controller since it is implemented as a singleton.
+   */
   private static AuthenticationController instance;
 
+  /**
+   * The UserRepository that deals with the adapters on a lower level.
+   */
   private UserRepository userRepository;
 
+  /**
+   * A reference to the current user logged in to the system.
+   */
   private User currentUser;
 
   private AuthenticationController() {
     userRepository = UserRepository.getInstance();
   }
 
+  /**
+   * Straightforward implementation of a singleton method to obtain the instance.
+   * @return the single instance of the class
+   */
   public static AuthenticationController getInstance() {
     if (instance == null) {
       instance = new AuthenticationController();
@@ -23,6 +40,12 @@ public class AuthenticationController {
     return instance;
   }
 
+  /**
+   * tests the given username/password combination against the database.
+   * @param name username entered by the user that wants to get authenticated
+   * @param password password entered by the user that wants to get authenticated
+   * @return true if authentication succeeds, false otherwise.
+   */
   public boolean authenticate(String name, String password) {
     try {
       User user = userRepository.findUserByName(name);
@@ -41,6 +64,10 @@ public class AuthenticationController {
     }
   }
 
+  /**
+   * commands the {@link UserRepository} to save the given {@link User} to the database.
+   * @param user the user to be saved
+   */
   public void addUser(User user) {
     try {
       System.out.println("Trying to create new account: " + user.name);
@@ -51,11 +78,22 @@ public class AuthenticationController {
     }
   }
 
+  /**
+   * adapts the {@link UserRepository} to the given adapter
+   * @param adapter the enum value representing an adapter
+   * @see Adapter
+   * @see brickingbad.services.authentication.IAuthenticationAdapter
+   * @return itself so that methods can be chained.
+   */
   public AuthenticationController adapt(Adapter adapter) {
     userRepository.adapt(adapter);
     return this;
   }
 
+  /**
+   *
+   * @return the user that is currently logged in
+   */
   public User getCurrentUser() {
     return currentUser;
   }
