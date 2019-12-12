393464a0d435cc72f631f108c30354479c6de4bf
egeerdogan <ege@erdogan.dev>
2019-12-12 17:18:10 +0300

    create authcontroller test class, add junit libs

---
+++ tests/AuthenticationControllerTest.java
@@ -0,0 +1,16 @@
+import brickingbad.controller.AuthenticationController;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AuthenticationControllerTest {
+
+  private AuthenticationController authenticationController;
+
+  @Before
+  public void setUp() {
+    authenticationController = AuthenticationController.getInstance();
+  }
+
+  // TODO: delete user
+
+}
