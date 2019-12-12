f2e02329cfd67b5d39dc30c04b4b58d8baa6645b
NursenaKoprucu <nursenakoprucu@gmail.com>
2019-12-12 18:38:37 +0300

    Brick Factory documentation

--- src/brickingbad/domain/game/brick/BrickFactory.java
+++ src/brickingbad/domain/game/brick/BrickFactory.java
@@ -2,11 +2,19 @@ package brickingbad.domain.game.brick;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-
+/**
+ * BrickingFactory is responsible for creating the desired brick type.
+ *
+ */
 public class BrickFactory {
-
+    /**
+     * Maintains the single instance of {@link BrickFactory} since it is implemented as a singleton.
+     */
     private static BrickFactory instance;
-
+    /**
+     * Straightforward implementation of a singleton method to obtain the instance.
+     * @return the single instance of the class
+     */
     public static BrickFactory getInstance() {
         if (instance == null) {
             instance = new BrickFactory();
@@ -18,6 +26,11 @@ public class BrickFactory {
 
     }
 
+    /**
+     * creates the simple brick type.
+     * @param n simple brick count that needs to be created.
+     * @return simpleBricks as an ArrayList.
+     */
     public ArrayList<Brick> createSimpleBricks(int n) {
         ArrayList<Brick> simpleBricks = new ArrayList<>();
         for (int i = 0; i<n; i++){
@@ -26,6 +39,11 @@ public class BrickFactory {
         return simpleBricks;
     }
 
+    /**
+     * creates the half metal brick type.
+     * @param n half metal brick count that needs to be created.
+     * @return halfMetalBrick as an ArrayList.
+     */
     public ArrayList<Brick> createHalfMetalBricks(int n) {
         ArrayList<Brick> halfMetalBricks = new ArrayList<>();
         for (int i = 0; i<n; i++) {
@@ -34,6 +52,11 @@ public class BrickFactory {
         return halfMetalBricks;
     }
 
+    /**
+     * creates the mine brick type.
+     * @param n mine brick count that needs to be created.
+     * @return mineBricks as an ArrayList.
+     */
     public ArrayList<Brick> createMineBricks(int n) {
         ArrayList<Brick> mineBricks = new ArrayList<>();
         for (int i = 0; i<n; i++) {
@@ -42,6 +65,11 @@ public class BrickFactory {
         return mineBricks;
     }
 
+    /**
+     * creates the wrapper brick type.
+     * @param n wrapper brick count that needs to be created.
+     * @return wrapperBricks as an ArrayList.
+     */
     public ArrayList<Brick> createWrapperBricks(int n) {
         ArrayList<Brick> wrapperBricks = new ArrayList<>();
         for (int i = 0; i<n; i++) {
@@ -50,6 +78,11 @@ public class BrickFactory {
         return wrapperBricks;
     }
 
+    /**
+     * creates brick for a required brick type
+     * @param className gives the required brick type
+     * @return brick
+     */
     public Brick createBrick(String className) {
         Brick brick;
         try {
