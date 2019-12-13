cf81d680e774ab1ea1de994c3e6f0a490b7f9e91
NursenaKoprucu <nursenakoprucu@gmail.com>
2019-12-13 12:51:12 +0300

    Vector documentation

--- src/brickingbad/domain/physics/Vector.java
+++ src/brickingbad/domain/physics/Vector.java
@@ -1,8 +1,17 @@
 package brickingbad.domain.physics;
 
+/**
+ * Vector is responsible for creating a vector.
+ */
 public class Vector {
 
+    /**
+     * the private value for horizontal line of the vector
+     */
     private double xval;
+    /**
+     * the private value for vertical line of the vector
+     */
     private double yval;
 
     public Vector() {setVector(0.0, 0.0);}
@@ -23,24 +32,46 @@ public class Vector {
         return yval;
     }
 
+    /**
+     * sums the vectors with respect to their x and y values
+     * @param c is a vector that will be added to the current vector
+     */
     public void addVector(Vector c){
         xval += c.getX();
         yval += c.getY();
     }
 
+    /**
+     * multiplies the vector by a given multiplicity
+     * @param mul is the multiplicity that will be multiplied by the vector
+     */
     public void multiplyVector(double mul) {
         xval *= mul;
         yval *= mul;
     }
 
+    /**
+     * sums the vectors with respect to their x and y values
+     * @param c is the vector that will be added to the current vector
+     * @return the summation as a Vector
+     */
     public Vector sum(Vector c) {
         return new Vector(xval + c.getX(), yval + c.getY());
     }
 
+    /**
+     *  multiplies the vector by a given multiplicity
+     * @param mul is the multiplicity that will be multiplied by the vector
+     * @return the product as a Vector
+     */
     public Vector product(double mul) {
         return new Vector(xval * mul, yval * mul);
     }
 
+    /**
+     * it converts the format to the string
+     * @return the vector as a String 
+     */
     @Override
     public String toString() {
         return String.format("Vector: (%f, %f)", xval, yval);
