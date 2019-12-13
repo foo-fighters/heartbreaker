cf15d23d2035f43029e685d05392c462ae61b6ed
egeerdogan <ege@erdogan.dev>
2019-12-13 13:18:16 +0300

    cretae physics engine test class

---
+++ tests/PhysicsEngineTest.java
@@ -0,0 +1,15 @@
+import brickingbad.domain.physics.PhysicsEngine;
+import org.junit.Before;
+
+public class PhysicsEngineTest {
+
+  private PhysicsEngine engine;
+
+  @Before
+  public void setUp() {
+    engine = PhysicsEngine.getInstance();
+  }
+
+  // TODO: tests
+
+}
