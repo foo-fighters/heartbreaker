d96566e9d56f5a7d264dd1c3c3a903500bace096
canardaaydin <33313579+canardaaydin@users.noreply.github.com>
2019-12-15 22:46:35 +0300

    documented brickFactory

--- src/brickingbad/controller/AuthenticationController.java
+++ src/brickingbad/controller/AuthenticationController.java
@@ -6,134 +6,136 @@ import brickingbad.services.authentication.UserRepository;
 
 /**
  * AuthenticationController is responsible of handling requests related to authentication.
- *
  */
 public class AuthenticationController {
-  /* OVERVIEW: creates a link between the ui events regarding authentication
+    /* OVERVIEW: creates a link between the ui events regarding authentication
                   and the service layer (UserRepository).
-   */
+    */
 
-  /**
-   * Maintains the single instance of {@link AuthenticationController} since it is implemented as a singleton.
-   */
-  private static AuthenticationController instance;
+    /**
+     * Maintains the single instance of {@link AuthenticationController} since it is implemented as a singleton.
+     */
+    private static AuthenticationController instance;
 
-  /**
-   * The {@link UserRepository} that deals with the adapters on a lower level.
-   */
-  private UserRepository userRepository;
+    /**
+     * The {@link UserRepository} that deals with the adapters on a lower level.
+     */
+    private UserRepository userRepository;
 
-  /**
-   * A reference to the current {@link User} logged in to the system.
-   */
-  private User currentUser;
+    /**
+     * A reference to the current {@link User} logged in to the system.
+     */
+    private User currentUser;
 
-  private AuthenticationController() {
-    userRepository = UserRepository.getInstance();
-  }
-
-  /**
-   * Straightforward implementation of a singleton method to obtain the instance.
-   * @return the single instance of the class
-   */
-  public static AuthenticationController getInstance() {
-    if (instance == null) {
-      instance = new AuthenticationController();
-    }
-    return instance;
-  }
-
-  /**
-   * tests the given username/password combination against the database.
-   * @param name username entered by the user that wants to get authenticated
-   * @param password password entered by the user that wants to get authenticated
-   * @return true if authentication succeeds, false otherwise.
-   */
-  public boolean authenticate(String name, String password) {
-    // REQUIRES: two non-null strings
-    // MODIFIES: currentUser
-    // EFFECTS:  returns the result of the authentication operations
-    try {
-      User user = userRepository.findUserByName(name);
-      if (user.password.equals(password)) {
-        System.out.println("Successful login: " + user.name);
-        currentUser = user;
-        System.out.println(toString());
-        return true;
-      } else {
-        System.out.println("Wrong password for: " + user.name);
-        return false;
-      }
-    } catch (IllegalArgumentException e) {
-      System.out.println("User doesn't exist: " + name);
-      // TODO: handle (show UI message - failed login)
-      return false;
-    }
-  }
-
-  /**
-   * commands the {@link UserRepository} to save the given {@link User} to the database.
-   * @param user the user to be saved
-   */
-  public boolean addUser(User user) {
-    // REQUIRES: a non-null User object
-    // MODIFIES: nothing
-    // EFFECTS:  the given user is written to the database.
-    try {
-      System.out.println("Trying to create new account: " + user.name);
-      return userRepository.addUser(user);
-    } catch (IllegalArgumentException e) {
-      System.out.println("User with name exists: " + user.name);
-      return false;
-    }
-  }
-
-  public void deleteUser(User user) {
-    // REQUIRES: a non-null User object
-    // MODIFIES: nothing
-    // EFFECTS:  the given user is deleted from the database.
-    userRepository.deleteUser(user);
-  }
-
-  /**
-   * adapts the {@link UserRepository} to the given adapter
-   * @param adapter the enum value representing an adapter
-   * @see Adapter
-   * @see brickingbad.services.authentication.IAuthenticationAdapter
-   * @return itself so that methods can be chained.
-   */
-  public AuthenticationController adapt(Adapter adapter) {
-    // REQUIRES: an Adapter enum value
-    // MODIFIES: nothing
-    // EFFECTS:  userRepository is adapted to the given adapter.
-    userRepository.adapt(adapter);
-    return this;
-  }
-
-  /**
-   *
-   * @return the user that is currently logged in
-   */
-  public User getCurrentUser() {
-    return currentUser;
-  }
-
-  @Override
-  public String toString() {
-    String message = "";
-    message += "AuthenticationController \n";
-    message += "\tadapted to: " + userRepository.getAdapter().getClass().getSimpleName() + "\n";
-    if (currentUser == null) {
-      message += "\tcurrent user: " + "---" + "\n";
-    } else {
-      message += "\tcurrent user: " + currentUser.name + "\n";
+    private AuthenticationController() {
+        userRepository = UserRepository.getInstance();
     }
 
-    return message;
-  }
+    /**
+     * Straightforward implementation of a singleton method to obtain the instance.
+     *
+     * @return the single instance of the class
+     */
+    public static AuthenticationController getInstance() {
+        if (instance == null) {
+            instance = new AuthenticationController();
+        }
+        return instance;
+    }
 
-  public boolean repOk() {
-    return true; // ?
-  }
+    /**
+     * tests the given username/password combination against the database.
+     *
+     * @param name     username entered by the user that wants to get authenticated
+     * @param password password entered by the user that wants to get authenticated
+     * @return true if authentication succeeds, false otherwise.
+     */
+    public boolean authenticate(String name, String password) {
+        // REQUIRES: two non-null strings
+        // MODIFIES: currentUser
+        // EFFECTS:  returns the result of the authentication operations
+        try {
+            User user = userRepository.findUserByName(name);
+            if (user.password.equals(password)) {
+                System.out.println("Successful login: " + user.name);
+                currentUser = user;
+                System.out.println(toString());
+                return true;
+            } else {
+                System.out.println("Wrong password for: " + user.name);
+                return false;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("User doesn't exist: " + name);
+            // TODO: handle (show UI message - failed login)
+            return false;
+        }
+    }
+
+    /**
+     * commands the {@link UserRepository} to save the given {@link User} to the database.
+     *
+     * @param user the user to be saved
+     */
+    public boolean addUser(User user) {
+        // REQUIRES: a non-null User object
+        // MODIFIES: nothing
+        // EFFECTS:  the given user is written to the database.
+        try {
+            System.out.println("Trying to create new account: " + user.name);
+            return userRepository.addUser(user);
+        } catch (IllegalArgumentException e) {
+            System.out.println("User with name exists: " + user.name);
+            return false;
+        }
+    }
+
+    public void deleteUser(User user) {
+        // REQUIRES: a non-null User object
+        // MODIFIES: nothing
+        // EFFECTS:  the given user is deleted from the database.
+        userRepository.deleteUser(user);
+    }
+
+    /**
+     * adapts the {@link UserRepository} to the given adapter
+     *
+     * @param adapter the enum value representing an adapter
+     * @return itself so that methods can be chained.
+     * @see Adapter
+     * @see brickingbad.services.authentication.IAuthenticationAdapter
+     */
+    public AuthenticationController adapt(Adapter adapter) {
+        // REQUIRES: an Adapter enum value
+        // MODIFIES: nothing
+        // EFFECTS:  userRepository is adapted to the given adapter.
+        userRepository.adapt(adapter);
+        return this;
+    }
+
+    /**
+     * @return the user that is currently logged in
+     */
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    @Override
+    public String toString() {
+        String message = "";
+        message += "AuthenticationController \n";
+        message += "\tadapted to: " + userRepository.getAdapter().getClass().getSimpleName() + "\n";
+        if (currentUser == null) {
+            message += "\tcurrent user: " + "---" + "\n";
+        } else {
+            message += "\tcurrent user: " + currentUser.name + "\n";
+        }
+
+        return message;
+    }
+
+    public boolean repOk() {
+        return true; // ?
+    }
 
 }
--- src/brickingbad/domain/game/brick/BrickFactory.java
+++ src/brickingbad/domain/game/brick/BrickFactory.java
@@ -2,11 +2,14 @@ package brickingbad.domain.game.brick;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+
 /**
  * BrickingFactory is responsible for creating the desired brick type.
- *
  */
 public class BrickFactory {
+    /* OVERVIEW: creates different kinds of bricks (simple brick, halfMetal brick,
+                                                        mine brick, wrapper brick).
+    */
     /**
      * Maintains the single instance of {@link BrickFactory} since it is implemented as a singleton.
      */
@@ -14,6 +17,7 @@ public class BrickFactory {
 
     /**
      * Straightforward implementation of a singleton method to obtain the instance.
+     *
      * @return the single instance of the class
      */
     public static BrickFactory getInstance() {
@@ -29,12 +33,16 @@ public class BrickFactory {
 
     /**
      * creates the simple brick type.
+     *
      * @param n simple brick count that needs to be created.
      * @return simpleBricks as an ArrayList.
      */
     public ArrayList<Brick> createSimpleBricks(int n) {
+        // REQUIRES: an integer greater than 0
+        // MODIFIES:
+        // EFFECTS:  returns an array of newly created bricks
         ArrayList<Brick> simpleBricks = new ArrayList<>();
-        for (int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             simpleBricks.add(new SimpleBrick());
         }
         return simpleBricks;
@@ -42,12 +50,16 @@ public class BrickFactory {
 
     /**
      * creates the half metal brick type.
+     *
      * @param n half metal brick count that needs to be created.
      * @return halfMetalBrick as an ArrayList.
      */
     public ArrayList<Brick> createHalfMetalBricks(int n) {
+        // REQUIRES: an integer greater than 0
+        // MODIFIES:
+        // EFFECTS:  returns an array of newly created bricks
         ArrayList<Brick> halfMetalBricks = new ArrayList<>();
-        for (int i = 0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             halfMetalBricks.add(new HalfMetalBrick());
         }
         return halfMetalBricks;
@@ -55,12 +67,16 @@ public class BrickFactory {
 
     /**
      * creates the mine brick type.
+     *
      * @param n mine brick count that needs to be created.
      * @return mineBricks as an ArrayList.
      */
     public ArrayList<Brick> createMineBricks(int n) {
+        // REQUIRES: an integer greater than 0
+        // MODIFIES:
+        // EFFECTS:  returns an array of newly created bricks
         ArrayList<Brick> mineBricks = new ArrayList<>();
-        for (int i = 0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             mineBricks.add(new MineBrick());
         }
         return mineBricks;
@@ -68,12 +84,16 @@ public class BrickFactory {
 
     /**
      * creates the wrapper brick type.
+     *
      * @param n wrapper brick count that needs to be created.
      * @return wrapperBricks as an ArrayList.
      */
     public ArrayList<Brick> createWrapperBricks(int n) {
+        // REQUIRES: integer greater than 0
+        // MODIFIES:
+        // EFFECTS:  returns an array of newly created bricks
         ArrayList<Brick> wrapperBricks = new ArrayList<>();
-        for (int i = 0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             wrapperBricks.add(new WrapperBrick());
         }
         return wrapperBricks;
@@ -81,11 +101,15 @@ public class BrickFactory {
 
     /**
      * creates brick for a required brick type
+     *
      * @param className gives the required brick type
      * @return brick
      */
     public Brick createBrick(String className) throws InvocationTargetException, NoSuchMethodException,
             ClassNotFoundException, InstantiationException, IllegalAccessException {
+        // REQUIRES: a non-null string representing a class name
+        // MODIFIES:
+        // EFFECTS:  returns a brick
         Brick brick = null;
         try {
             brick = (Brick) Class.forName(className).getDeclaredConstructor().newInstance();
