d549c9bd1ec3d5dd616760c7d5709506c27f0935
zeynepdundar <idundar17@ku.edu.tr>
2019-12-15 20:18:30 +0300

    BrickFactoryTest assert fixed

--- tests/BrickFactoryTest.java
+++ tests/BrickFactoryTest.java
@@ -1,20 +1,25 @@
 import brickingbad.domain.game.brick.*;
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
-
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 
 public class BrickFactoryTest {
 
     private BrickFactory brickFactory;
+    private Brick simpleBrick;
+    private Brick halfMetalBrick;
+    private Brick mineBrick;
+    private Brick wrapperBrick;
 
     @Before
     public void setUp() {
         brickFactory = brickingbad.domain.game.brick.BrickFactory.getInstance();
+        simpleBrick = brickFactory.createBrick("brickingbad.domain.game.brick.SimpleBrick");
+        mineBrick =  brickFactory.createBrick("brickingbad.domain.game.brick.MineBrick");
+        wrapperBrick =  brickFactory.createBrick("brickingbad.domain.game.brick.WrapperBrick");
+        halfMetalBrick =brickFactory.createBrick("brickingbad.domain.game.brick.HalfMetalBrick");
     }
 
     @Test
@@ -23,48 +28,50 @@ public class BrickFactoryTest {
     }
 
     @Test
-    public void isBrickReturnedByTypeTest() throws ClassNotFoundException, NoSuchMethodException,
-            InstantiationException, IllegalAccessException, InvocationTargetException {
-        Brick simpleBrick = brickFactory.createBrick("brickingbad.domain.game.brick.SimpleBrick");
-        Brick mineBrick =  brickFactory.createBrick("brickingbad.domain.game.brick.MineBrick");
-        Brick wrapperBrick = brickFactory.createBrick("brickingbad.domain.game.brick.WrapperBrick");
-        Brick halfMetalBrick = brickFactory.createBrick("brickingbad.domain.game.brick.HalfMetalBrick");
-        assertTrue(simpleBrick instanceof SimpleBrick);
-        assertTrue(halfMetalBrick instanceof HalfMetalBrick);
-        assertTrue(mineBrick instanceof MineBrick);
-        assertTrue(wrapperBrick instanceof WrapperBrick);
-    }
+    public void isABrickReturnedByType() throws Exception {
 
-    @Test(expected = ClassNotFoundException.class)
-    public void createBrickErrorTest() throws ClassNotFoundException, NoSuchMethodException,
-            InstantiationException, IllegalAccessException, InvocationTargetException {
-        Brick errorBrick = brickFactory.createBrick("errorString");
+        /*boolean simpleBrickCheck = (simpleBrick instanceof SimpleBrick);
+        boolean mineBrickCheck = (mineBrick instanceof MineBrick);
+        boolean wrapperBrickCheck = (wrapperBrick instanceof WrapperBrick);
+        boolean halfMetalBrickCheck = (halfMetalBrick instanceof HalfMetalBrick);*/
+
+            assertTrue(simpleBrick instanceof SimpleBrick);
+            assertTrue(mineBrick instanceof MineBrick);
+            assertTrue(wrapperBrick instanceof WrapperBrick);
+            assertTrue(halfMetalBrick instanceof HalfMetalBrick);
+
     }
 
     @Test
     public void isBrickListReturnedByTypeTest() {
-        int expectedSize = 10;
-        ArrayList<Brick> simpleBrickList = new ArrayList<>(brickFactory.createSimpleBricks(expectedSize));
-        ArrayList<Brick> mineBrickList = new ArrayList<>(brickFactory.createMineBricks(expectedSize));
-        ArrayList<Brick> halfMetalBrickList = new ArrayList<>(brickFactory.createHalfMetalBricks(expectedSize));
-        ArrayList<Brick> wrapperBrickList = new ArrayList<>(brickFactory.createWrapperBricks(expectedSize));
+        int n = 100;
+        ArrayList simpleBrickList = new ArrayList<>(brickFactory.createSimpleBricks(n));
+        ArrayList mineBrickList = new ArrayList<>(brickFactory.createMineBricks(n));
+        ArrayList halfMetalBrickList = new ArrayList<>(brickFactory.createHalfMetalBricks(n));
+        ArrayList wrapperBrickList = new ArrayList<>(brickFactory.createWrapperBricks(n));
 
-        assertEquals(expectedSize, simpleBrickList.size());
-        assertEquals(expectedSize, mineBrickList.size());
-        assertEquals(expectedSize, halfMetalBrickList.size());
-        assertEquals(expectedSize, wrapperBrickList.size());
+        isBricklistReturnedByType(SimpleBrick.class, simpleBrickList, n);
+        isBricklistReturnedByType(MineBrick.class, mineBrickList , n);
+        isBricklistReturnedByType(WrapperBrick.class, wrapperBrickList, n);
+        isBricklistReturnedByType(HalfMetalBrick.class, halfMetalBrickList, n);
+    }
 
-        for(Brick brick: simpleBrickList) {
-            assertTrue(brick instanceof SimpleBrick);
+
+    public void isBricklistReturnedByType(Class Brick, ArrayList brickList, int n) {
+        boolean checkBrick = false;
+        //ArrayList testList = new ArrayList<>(CreateBrickListTest(n, brickClass));
+        if(n != brickList.size()){
+            assertTrue("Size does not match with the test size", false);
+            return;
         }
-        for(Brick brick: mineBrickList) {
-            assertTrue(brick instanceof MineBrick);
-        }
-        for(Brick brick: halfMetalBrickList) {
-            assertTrue(brick instanceof HalfMetalBrick);
-        }
-        for(Brick brick: wrapperBrickList) {
-            assertTrue(brick instanceof WrapperBrick);
+
+        for (int i = 0; i < brickList.size(); i++) {
+            if(brickList.get(i) instanceof Brick){
+                checkBrick = true;
+            }else{
+                assertTrue("Object is not an instance of" + Brick.getName()+ ".", checkBrick);
+            }
+            checkBrick = false;
         }
     }
 }
